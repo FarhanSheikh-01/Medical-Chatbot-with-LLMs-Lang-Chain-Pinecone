817083f2958ace93772c66acaf2c8078a3f07e28
FarhanSheikh-01 <sheikhfarhan001.sf@gmail.com>
2025-08-25 16:16:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sheikh Farhan",
+    author_email="sheikhfarhan001.sf@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
